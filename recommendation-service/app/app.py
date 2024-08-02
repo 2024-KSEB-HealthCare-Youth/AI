380ea1c05ed8c4b4af73d828eeba35b46d3e0671
tlsgy <tlsgytlr001@gmail.com>
2024-08-02 15:37:19 +0900

    refactor: depth-model develop

--- recommendation-service/app/app.py
+++ recommendation-service/app/app.py
@@ -1,7 +1,8 @@
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 from flask import Flask, request, jsonify
-from flask_restx import Resource, Api, Namespace
+from flask_restx import Resource, Api, Namespace, reqparse
 from flask_cors import CORS
 from base_skintype import analyze_base_skintype
 from depth_skintype import analyze_depth_skintype
@@ -9,29 +10,34 @@ from CBR import get_recommendations
 from nutr_recommendation import get_recommended_nutrs
 from collaborative_filtering import get_recommendations_collabo
 from resultImage import make_Image
-import logging
-logging.basicConfig(level=logging.DEBUG)
-
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
 SkinAnalysis = Namespace('SkinAnalysis')
+parser = reqparse.RequestParser()
+parser.add_argument('file', location='files', type='file', required=True, help='File to upload')
 
-'''
+
 @SkinAnalysis.route('')
 class SkinAnalysisResource(Resource):
+    @SkinAnalysis.expect(parser)  # reqparse를 사용한 파라미터 기대
     def post(self):
         file = request.files.get('file')
+
         if not file:
             return jsonify({'error': 'No file provided'}), 400
 
         try:
+            # 파일 정보 출력 (디버그 목적)
+            print(f"Received file: {file.filename}, Size: {len(file.read())} bytes")
+            file.seek(0)  # 파일 포인터를 처음으로 되돌림
+
             # Base Skin Type 분석
             base_skin_type, base_probabilities = analyze_base_skintype(file)
 
-            # 파일 포인터를 처음으로 되돌립니다
+            # 파일 포인터를 처음으로 되돌림
             file.seek(0)
 
             # Depth Skin Type 분석
@@ -40,13 +46,13 @@ class SkinAnalysisResource(Resource):
             # CBR 추천
             recommendations = get_recommendations(base_skin_type, depth_skin_type)
 
-            #collaborative-filtering 추천
+            # Collaborative Filtering 추천
             recommendations_collabo = get_recommendations_collabo(base_skin_type)
 
-            # nutr 추천
+            # Nutritional 추천
             nutrs_recommendations = get_recommended_nutrs(base_skin_type, depth_skin_type)
 
-            #resultImage 생성
+            # resultImage 생성
             resultImage_str = make_Image(base_probabilities, depth_probabilities)
 
             # 결과 집합 초기화
@@ -58,20 +64,19 @@ class SkinAnalysisResource(Resource):
                 cosNames.append(rec['title'])
                 cosPaths.append(rec['imgurl'])
 
-                # Collaborative Filtering 추천 추가
-                added_count = 0
-                for rec in recommendations_collabo:
-                    if added_count >= 3:  # 최대 3개 항목을 추가
-                        break
-                    if rec['title'] not in cosNames:  # 중복 제거
-                        cosNames.append(rec['title'])
-                        cosPaths.append(rec['imgurl'])
-                        added_count += 1
+            # Collaborative Filtering 추천 추가
+            added_count = 0
+            for rec in recommendations_collabo:
+                if added_count >= 3:  # 최대 3개 항목을 추가
+                    break
+                if rec['title'] not in cosNames:  # 중복 제거
+                    cosNames.append(rec['title'])
+                    cosPaths.append(rec['imgurl'])
+                    added_count += 1
 
-                    # nutr 추천
-                    nutrNames = [nutr['title'] for nutr in nutrs_recommendations]
-                    nutrPaths = [nutr['url'] for nutr in nutrs_recommendations]
-
+            # Nutritional 추천 추가
+            nutrNames = [nutr['title'] for nutr in nutrs_recommendations]
+            nutrPaths = [nutr['url'] for nutr in nutrs_recommendations]
 
             response = {
                 'cosNames': cosNames,
@@ -85,86 +90,11 @@ class SkinAnalysisResource(Resource):
 
             return jsonify(response)
         except Exception as e:
+            # 예외 처리 (보안 고려 필요)
             return jsonify({'error': str(e)}), 500
 
-'''
-@SkinAnalysis.route('')
-class SkinAnalysisResource(Resource):
-    def post(self):
-        logging.debug("Received POST request")
-        file = request.files.get('file')
-        if not file:
-            logging.error("No file provided")
-            return jsonify({'error': 'No file provided'}), 400
-
-        try:
-            logging.debug("Starting base skin type analysis")
-            base_skin_type, base_probabilities = analyze_base_skintype(file)
-            logging.debug(f"Base skin type: {base_skin_type}")
-
-            file.seek(0)
-            logging.debug("Starting depth skin type analysis")
-            depth_skin_type, depth_probabilities = analyze_depth_skintype(file)
-            logging.debug(f"Depth skin type: {depth_skin_type}")
-
-            logging.debug("Getting recommendations")
-            recommendations = get_recommendations(base_skin_type, depth_skin_type)
-            logging.debug(f"Recommendations: {recommendations}")
-
-            logging.debug("Getting collaborative filtering recommendations")
-            recommendations_collabo = get_recommendations_collabo(base_skin_type)
-            logging.debug(f"Collaborative filtering recommendations: {recommendations_collabo}")
-
-            logging.debug("Getting nutr recommendations")
-            nutrs_recommendations = get_recommended_nutrs(base_skin_type, depth_skin_type)
-            logging.debug(f"Nutr recommendations: {nutrs_recommendations}")
-
-            logging.debug("Making result image")
-            resultImage_str = make_Image(base_probabilities, depth_probabilities)
-            logging.debug("Result image created")
-
-            # 결과 집합 초기화
-            cosNames = []
-            cosPaths = []
-
-            # CBR 추천 추가
-            for rec in recommendations:
-                cosNames.append(rec['title'])
-                cosPaths.append(rec['imgurl'])
-
-                # Collaborative Filtering 추천 추가
-                added_count = 0
-                for rec in recommendations_collabo:
-                    if added_count >= 3:  # 최대 3개 항목을 추가
-                        break
-                    if rec['title'] not in cosNames:  # 중복 제거
-                        cosNames.append(rec['title'])
-                        cosPaths.append(rec['imgurl'])
-                        added_count += 1
-
-                    # nutr 추천
-                    nutrNames = [nutr['title'] for nutr in nutrs_recommendations]
-                    nutrPaths = [nutr['url'] for nutr in nutrs_recommendations]
-
-            logging.debug("Preparing response")
-            response = {
-                'cosNames': cosNames,
-                'cosPaths': cosPaths,
-                'nutrNames': nutrNames,
-                'nutrPaths': nutrPaths,
-                'simpleSkin': base_skin_type,
-                'expertSkin': depth_skin_type,
-                'resultImage': resultImage_str
-            }
-            logging.debug(f"Response: {response}")
-
-            return jsonify(response)
-        except Exception as e:
-            logging.exception("An error occurred")
-            return jsonify({'error': str(e)}), 500
-        
 
 api.add_namespace(SkinAnalysis, '/upload')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True) #debug 필요하면 debug=True 넣어주기 (실제 서버에서는 안넣는 것이 좋음)
+    app.run(host='0.0.0.0', port=5000)  # debug 필요하면 debug=True 넣어주기 (실제 서버에서는 안넣는 것이 좋음)
--- recommendation-service/app/depth_skintype.py
+++ recommendation-service/app/depth_skintype.py
@@ -1,18 +1,34 @@
 from ultralytics import YOLO
 from PIL import Image
 import io
-#from flask import app
+import numpy as np
+import os
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+pt_path = os.path.join(base_dir, '../data/best.pt')
+image_save_path = os.path.join(base_dir, '../data/uploaded_image.jpg')  # 이미지를 저장할 경로
+
 
 def analyze_depth_skintype(file):
+    file.seek(0)
+    print('depth 도달!')
     # YOLO 모델 불러오기
-    model = YOLO('best.pt')
+    model = YOLO(pt_path)
+    print('모델 로딩 성공!')
 
     # 파일 객체로부터 이미지 열기
     img = Image.open(io.BytesIO(file.read()))
+    print('이미지 오픈 성공!')
     img = img.resize((256, 256))
 
-    # 이미지에 대한 예측 수행
-    results = model(img)
+    # 이미지를 저장
+    img.save(image_save_path)
+    print('이미지 저장 성공!')
+
+    # 이미지 경로를 사용하여 예측 수행
+    print('수행 시작!')
+    results = model(image_save_path)
+    print('수행 끝!')
 
     # 클래스 이름 리스트 (모델에 정의된 순서대로)
     class_names = ['acne', 'dry', 'normal', 'oily', 'wrinkles']
@@ -36,8 +52,9 @@ def analyze_depth_skintype(file):
                     detected_conditions.add('wrinkles')
                     probabilities['wrinkles'] = max(probabilities['wrinkles'], confidence)
 
-    #app.logger.info(f"depth_skin_type: {list(detected_conditions)} (type: {type(list(detected_conditions))})")
-    #app.logger.info(f"depth_probabilities: {probabilities} (type: {type(probabilities)})")
+    # 저장된 이미지를 삭제
+    os.remove(image_save_path)
+    print('이미지 삭제 성공!')
 
     return list(detected_conditions), probabilities
 
